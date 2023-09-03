373432a305193fe6a80112e7e43d131b5bb58ce3
yana <yanaposelenceva@gmail.com>
2023-09-03 20:16:30 +0300

    add files

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        massage = request.POST.get('massage')
+        print(f'{name} ({email}): {massage}')
+    return render(request, 'catalog/contacts.html')
+
+
