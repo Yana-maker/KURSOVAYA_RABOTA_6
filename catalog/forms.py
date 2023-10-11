b0bf893993f7e06c0e77d37a87fae693ca0e397b
Yana-maker <134791055+Yana-maker@users.noreply.github.com>
2023-10-11 10:17:38 +0300

    fixes HW6

--- catalog/forms.py
+++ catalog/forms.py
@@ -8,29 +8,38 @@ class StyleFormMixin:
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
 
+
+
 class ProductForm(StyleFormMixin, forms.ModelForm):
+
+    forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
+                            'радар']
+
     class Meta:
         model = Product
         fields = '__all__'
 
-    def clean_product_name(self):
-        cleaned_data = self.cleaned_data['product_name']
-        if 'казино' in cleaned_data:
-            raise forms.ValidationError('подобные продукты нельзя создавать')
-        return cleaned_data
+    def clean_name(self):
+        name = self.cleaned_data['name'].lower()
+
+        for word in self.forbidden_words:
+            if word in name:
+                raise forms.ValidationError(f"Нельзя использовать слово '{word}' в названии продукта.")
+        return name
 
     def clean_product_description(self):
-        cleaned_data = self.cleaned_data['product_description']
-        if 'казино' in cleaned_data:
-            raise forms.ValidationError('подобные слова нельзя вносить в описание')
-        return cleaned_data
+        name = self.cleaned_data['product_description'].lower()
+
+        for word in self.forbidden_words:
+            if word in name:
+                raise forms.ValidationError(f"Нельзя использовать в описании слово '{word}' в названии продукта.")
+        return name
 
 
 class ContactsForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Contacts
         fields = '__all__'
-
 
     def clean_email(self):
         cleaned_data = self.cleaned_data['email']
